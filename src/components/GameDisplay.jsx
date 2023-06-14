42c9cb5879a564be4638cf63fc3fe4f40fb25b90
Tedros Alem <tedsaasfaha@gmail.com>
2023-06-15 01:39:55 +0300

    updated added a grid for moves & scores & a Start button

--- src/components/GameDisplay.jsx
+++ src/components/GameDisplay.jsx
@@ -1,8 +1,8 @@
-import { Grid } from '@mui/material';
+import { Box, Button, Container, Grid } from '@mui/material';
 import React from 'react';
 import { GameCard } from './GameCard';
 
-export const GameDisplay = ( { cards, onClick }) => {
+export const GameDisplay = ( { cards, onClick, score, moves }) => {
 
     const handleOnClick = () => {
 
@@ -13,13 +13,34 @@ export const GameDisplay = ( { cards, onClick }) => {
                 {
                     cards.map(card => {
                         return (
-                            <Grid item xs={3}>
+                            <Grid item xs={3} key={card.id}>
                                 <GameCard card={card} onClick={onClick} />
                             </Grid>
                         )
                     })
                 }             
             <Grid item></Grid> 
+            <Grid item xs={12}>
+                <>
+                <Container sx={{
+                    padding: 1,
+                    border: 1,
+                    borderRadius: 1,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-around'
+                }}       
+                >
+                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        Moves: {moves}
+                    </Box>   
+                    <Button>Start Over</Button> 
+                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        Score: {score}
+                    </Box>
+                </Container>  
+            </>  
+            </Grid> 
         </Grid>
     )
 }
